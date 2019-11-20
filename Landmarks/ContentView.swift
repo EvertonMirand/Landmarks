ab6614879be038c0183fcf22722ad11e95c2e5af
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-11-19 22:08:21 -0300

    First commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Everton Miranda Vitório on 19/11/19.
+//  Copyright © 2019 Everton Miranda Vitório. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            .foregroundColor(Color.black)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
